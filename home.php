e2ff115af2347e249c4ff94635944142b1210c85
fercardona18 <lcardonac5@miumg.edu.gt>
2023-04-20 21:14:48 -0600

    start proyect

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi página de ejemplo</title>
+</head>
+<body>
+    <header>
+        <h1>Bienvenidos a mi página de ejemplo</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Acerca de</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h2>Sección 1</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed lacus tortor. Suspendisse potenti. Fusce sed libero id lorem fermentum malesuada. Quisque iaculis, augue ac eleifend pharetra, ipsum nisi feugiat lectus, non hendrerit quam augue ac nibh. Donec at tellus sit amet orci placerat dapibus non sed nisi. In hac habitasse platea dictumst. Nullam aliquet turpis at libero porttitor rhoncus. </p>
+
+        <h2>Sección 2</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam bibendum blandit dui at iaculis. Aliquam erat volutpat. Ut ultricies est vitae quam tristique bibendum. Vestibulum lacinia nisl at quam iaculis, sit amet euismod libero aliquam. Aenean suscipit quam purus, in pharetra est fermentum at. In hac habitasse platea dictumst. </p>
+    </main>
+    <footer>
+        <p>Derechos reservados &copy; 2023</p>
+    </footer>
+</body>
+</html>
